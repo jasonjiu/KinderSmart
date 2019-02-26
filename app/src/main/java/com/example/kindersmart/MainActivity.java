518c65cbcaa77b7e8ebed5f0ef0be279dc8c0992
pharosmobile <forpharosmobile@gmail.com>
2019-02-26 10:46:46 +0700

    test

--- app/src/main/java/com/example/kindersmart/MainActivity.java
+++ app/src/main/java/com/example/kindersmart/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.kindersmart;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
